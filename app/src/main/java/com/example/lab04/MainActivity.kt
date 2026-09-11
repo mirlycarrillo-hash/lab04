751bf0a8637258e984ae2f013087f223bb672c1e
Mirly Carrillo <mirly.carrillo@tecsup.edu.pe>
2026-09-11 13:08:11 -0500

    feat: add Card component

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab04.ui.theme.Lab04Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab04Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainContent(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +34,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        ViewCard()
+        Spacer(modifier = Modifier.height(16.dp))
+        ViewButton()
+        Spacer(modifier = Modifier.height(16.dp))
+        ViewSwitch()
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Lab04Theme {
-        Greeting("Android")
+fun ViewCard() {
+    Card {
+        Text(text = "Componente Card", modifier = Modifier.padding(16.dp))
     }
+}
+
+@Composable
+fun ViewButton() {
+    FloatingActionButton(onClick = {}) {
+        Text(text = "FAB")
+    }
+}
+
+@Composable
+fun ViewSwitch() {
+    Switch(checked = true, onCheckedChange = {})
 }
